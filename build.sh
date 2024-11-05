7c8246973b754c3ee21917f6355e173200147d6a
chriswinsatlife <1500666+chriswinsatlife@users.noreply.github.com>
2024-11-06 02:36:04 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-# Run normal build command
+# Download and install a minimal quarto
+QUARTO_VERSION="1.6.32"
+wget -q https://github.com/quarto-dev/quarto-cli/releases/download/v${QUARTO_VERSION}/quarto-${QUARTO_VERSION}-linux-amd64.deb
+dpkg -x quarto-${QUARTO_VERSION}-linux-amd64.deb ./quarto-temp
+export PATH="$PWD/quarto-temp/usr/local/bin:$PATH"
+quarto --version
+
+# Process markdown files with Quarto
+quarto render ai_slides.md -t revealjs
+
+# Run normal build
 npm install
 npm run build --if-present
