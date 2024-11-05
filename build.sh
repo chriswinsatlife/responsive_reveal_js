35de3be98d7897f41634114ae29d4c040ac3cc3d
chriswinsatlife <1500666+chriswinsatlife@users.noreply.github.com>
2024-11-06 02:13:08 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Install Pandoc using a smaller binary download
-PANDOC_VERSION="3.1.11"
+# Install Pandoc using the latest version
+PANDOC_VERSION="3.1.9"  # This is the latest stable version as of November 2024
 wget "https://github.com/jgm/pandoc/releases/download/${PANDOC_VERSION}/pandoc-${PANDOC_VERSION}-linux-amd64.deb"
 dpkg -x pandoc-${PANDOC_VERSION}-linux-amd64.deb pandoc-temp
 export PATH="$PWD/pandoc-temp/usr/bin:$PATH"
