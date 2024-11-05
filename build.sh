6e409d210431c871a3f566e12111781c8a9562a8
chriswinsatlife <1500666+chriswinsatlife@users.noreply.github.com>
2024-11-06 02:22:07 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Install Pandoc using the correct version
-PANDOC_VERSION="3.5"
-wget "https://github.com/jgm/pandoc/releases/download/${PANDOC_VERSION}/pandoc-${PANDOC_VERSION}-linux-amd64.tar.gz"
-tar xvzf pandoc-${PANDOC_VERSION}-linux-amd64.tar.gz
-export PATH="$PWD/pandoc-${PANDOC_VERSION}/bin:$PATH"
-pandoc --version
-
 # Run normal build command
 npm install
 npm run build --if-present
